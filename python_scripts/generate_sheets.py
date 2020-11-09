6861a66483430820e587dc146ae3be87fd58eca3
jvmanne <jenny.v.manne@gmail.com>
2020-11-09 23:42:56 +0100

    add stars

--- python_scripts/generate_sheets.py
+++ python_scripts/generate_sheets.py
@@ -72,6 +72,7 @@ add_sprite(spriteSheet, 'img/asteroid-2.png')
 add_sprite(spriteSheet, 'img/asteroid-3.png')
 add_sprite(spriteSheet, 'img/laser-2.png')
 add_sprite(spriteSheet, 'img/gameover-1.png')
+add_sprite(spriteSheet, 'img/stars-1.png')
 
 save_sheets()
 
